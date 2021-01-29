9062a41fca47a258d434616dc686ff0e020f56c1
CoolandHot <coolandhot2006@qq.com>
2021-01-29 13:00:43 +0800

    use class objects

--- javRename.py
+++ javRename.py
@@ -6,9 +6,9 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-SEARCHURL = 'https://avmoo.host/cn/search/'
 reNameOrder = ['actress', 'javCode', 'title', 'publishDate']
 downimg = False
+useJavBus = True # if False, use avmoo
 
 
 PROXY = {"http": "socks5://127.0.0.1:1099", "https": "socks5://127.0.0.1:1099"}
@@ -16,65 +16,79 @@ HEADER = {
     "user-agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
     "accept": "application/json, text/javascript, */*; q=0.01",
-    "accept-language": "zh-CN,zh;q=0.9",
-    "dnt": "1",
-    "Connection": "keep-alive",
-    # "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    # "x-requested-with": "XMLHttpRequest",
-    'content-length': '0',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors'
 }
 
+class basic_class: # this class applies to both javBus and avmoo
+    def __init__(self, javCode):
+        self.javID = javCode
+        self.title, self.publishDate, self.heroine = None, None, None
+        self.img, self.request_url= None, None
+        self.finalstr = None # store the new name
+    def searchTitle(self, link):
+        try:
+            response = requests.get(link, headers=HEADER, allow_redirects=True, proxies=PROXY, verify=False)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')            
+                self.javID = soup.select_one('span.header').findNext('span').text
+                self.title = soup.select_one('h3').text.split(self.javID+' ')[1]
+                self.publishDate = re.search(r'\d{4}-\d{2}-\d{2}', soup.select('span.header')[1].parent.text).group(0)
+                self.findActress(soup) # find heroines
+                if downimg:
+                    imgurl = soup.select_one('.bigImage')['href'] # find the cover
+                    imgResponse = requests.get(imgurl, headers=HEADER, allow_redirects=True, proxies=PROXY) # download cover
+                    self.img = imgResponse.content
+                renameStr = {'actress': self.heroine,
+                            'javCode': self.javID,
+                            'title': self.title,
+                            'publishDate': self.publishDate}
+                neededOrder = [renameStr[i] for i in reNameOrder] # rename by the set order
+                self.finalstr = neededOrder[0]+'-['+']-['.join(neededOrder[1:4])+']'
+        except Exception as e:
+            print(e)
+    def searchID(self):
+        try:
+            response = requests.get(self.request_url, headers=HEADER, allow_redirects=True, proxies=PROXY, verify=False)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                links = soup.select('.movie-box')
+                if len(links)==1: # request returns only one result
+                    self.searchTitle(links[0]['href'])
+                else:
+                    javCode = self.javID.replace('-', '') # replace any '-' for later comparison
+                    for link in links:
+                        javID = link.select_one('date').text.replace('-', '') # acquire each result's javID
+                        if javCode==javID: # find the one with exact match
+                            self.searchTitle(link['href'])
+        except Exception as e:
+            print(e)
+    def findActress(self, soup):
+        return
 
-def findActress(soup):
-    avatars = soup.select('.avatar-box')
-    actresses = [avatar.select_one('span').text for avatar in avatars]
-    if len(actresses):
-        actress = ' '.join(actresses)
-    else:
-        actress = 'unknown'
-    return actress
-    
-def searchTitle(link):
-    finalstr = None
-    imgResponse = None
-    try:
-        response = requests.get(link, headers=HEADER, allow_redirects=True, proxies=PROXY, verify=False)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')            
-            javCode = soup.select_one('span.header').findNext('span').text
-            title = soup.select_one('h3').text.split(javCode+' ')[1]
-            publishDate = re.search(r'\d{4}-\d{2}-\d{2}', soup.select('span.header')[1].parent.text).group(0)
-            actress = findActress(soup)
-            imgurl = soup.select_one('.bigImage')['href']
-            imgResponse = requests.get(imgurl, headers=HEADER, allow_redirects=True, proxies=PROXY)
-            renameStr = {'actress': actress, 'javCode': javCode, 'title': title, 'publishDate': publishDate}
-            neededOrder = [renameStr[i] for i in reNameOrder]
-            finalstr = neededOrder[0]+'-['+']-['.join(neededOrder[1:4])+']'
-    except Exception as e:
-        print(e)
-    return finalstr, imgResponse
+class javBus(basic_class):
+    def __init__(self, javCode):
+        super().__init__(javCode)
+        self.request_url = 'https://www.javbus.com/search/'+self.javID
+    def findActress(self, soup):
+        avatars = soup.select('.star-name')
+        actresses = [avatar.select_one('a').text for avatar in avatars] # there could be multiple heroines
+        if len(actresses):
+            self.heroine = ' '.join(actresses)
+        else:
+            self.heroine = 'unknown'
 
-def searchID(javCode):
-    request_url = SEARCHURL+javCode
-    try:
-        response = requests.get(request_url, headers=HEADER, allow_redirects=True, proxies=PROXY, verify=False)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            links = soup.select('.movie-box')
-            if len(links)==1:
-                return searchTitle(links[0]['href'])
-            else:
-                javCode = javCode.replace('-', '')
-                for link in links:
-                    javID = link.select_one('date').text.replace('-', '')
-                    if javCode==javID:
-                        return searchTitle(link['href'])
-                return None, None
-    except Exception as e:
-        print(e)
+class avmoo(basic_class):
+    def __init__(self, javCode):
+        super().__init__(javCode)
+        self.request_url = 'https://avmoo.cyou/cn/search/'+self.javID
+    def findActress(self, soup):
+        avatars = soup.select('.avatar-box')
+        actresses = [avatar.select_one('span').text for avatar in avatars]
+        if len(actresses):
+            self.heroine = ' '.join(actresses)
+        else:
+            self.heroine = 'unknown'
+
+
 
 def javRe(fullpath):
     splitstr = os.path.split(fullpath.rstrip(os.path.sep))
@@ -82,16 +96,22 @@ def javRe(fullpath):
     matchObj = re.search(r"\.[A-Za-z0-9]{3,10}$", filename0)
     suffix = matchObj.group(0)
     filename = filename0[0:matchObj.span()[0]]
-    javCode = re.search(r'(?![^A-Za-z])?[A-Za-z]{2,5}-?\d{3,5}(?=\D)?', filename)
-    if javCode is not None:
-        newName, imgResponse = searchID(javCode.group(0))
+    javCode = re.search(r'(?![^A-Za-z])?[A-Za-z]{2,5}-\d{3,5}(?=\D)?', filename)
+    if javCode is None:
+        javCode = re.search(r'(?![^A-Za-z])?[A-Za-z]{2,5}-?\d{3,5}(?=\D)?', filename)
+
+    if useJavBus:
+        searchResult = javBus(javCode.group(0)) # use javBus website
     else:
-        newName = None
+        searchResult = avmoo(javCode.group(0))
+    searchResult.searchID()
+    newName, imgResp = searchResult.finalstr, searchResult.img
+
     if newName is not None:
         try:
             os.rename(fullpath, os.path.join(basepath, newName+suffix))
-            if downimg:                
-                open(os.path.join(basepath, newName+'.jpg'), 'wb').write(imgResponse.content)
+            if downimg:
+                open(os.path.join(basepath, newName+'.jpg'), 'wb').write(imgResp)
         except Exception as e:
             print(e)
             print('fail on', filename)
--- javRename拖放文件.bat
+++ javRename拖放文件.bat
@@ -5,11 +5,11 @@ set count=1
 :loop
 REM echo 绝对路径: %~1
 REM echo 文件路径: %~dp1
-REM echo 文件全名: %~nx1
+REM echo 文件名+扩展: %~nx1
 REM echo 文件名： %~n1
 REM echo 扩展名： %~x1
 echo -----------------------------
-python javRename.py "%~1"
+python javRename_v7.py "%~1"
 shift
 set /a count+=1
 if not [%1]==[] goto loop
